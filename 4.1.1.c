23d458a02130432165f1fefd9809b09a2d7713af
Nur <2020975359@isiswa.uitm.edu.my>
2020-11-22 15:01:27 +0000

    Commit for 4.1.1

--- 4.1.1.c
+++ 4.1.1.c
@@ -4,25 +4,35 @@
 
 int main(int argc, char *argv[])
 {
-	int socket_desc;
+	int socket_desc'
 	struct sockaddr_in server;
+	char *message;
 
 	//create socket
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_desc == -1)
+	if(socket_desc ==-1)
 	{
 		printf("could not create socket");
 	}
-	server.sin_addr.s_addr = inet_addr("192.168.0.162"); //server ip address
+
+	server.sin_addr.s_addr = inet_addr("192.168.0.162");
 	server.sin_family = AF_INET;
 	server.sin_port = htons(22);
 
 	//connect to remote server
-	if(connect(socket_desc, (struct sockaddr*)&server, sizeof(server)) <0)
+	if(connect(socket_desc, (struct sockaddr *)&server, sizeof(server))<0)
 	{
 		puts("connect error");
 		return 1;
 	}
-	puts("connected");
+	puts ("connected");
+	//send some data
+	message =  "connect";
+	if(send(socket_desc, message, strlen(message),0)<0)
+	{
+		puts("send failed");
+		return 1;
+	}
+	puts("data send");
 	return 0;
 }
