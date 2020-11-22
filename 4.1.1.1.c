c780bffb4acb83f9033888333851707f47e0952b
Nur <2020975359@isiswa.uitm.edu.my>
2020-11-22 15:28:48 +0000

    Commit for 4.1.1.1

---
+++ 4.1.1.1.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+
+int main(int argc, char *argv[])
+{
+	int socket_desc;
+	struct sockaddr_in server;
+	char *message;
+
+	//create socket
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_desc == -1)
+	{
+		printf("could not create socket");
+	}
+	
+	server.sin_addr.s_addr = inet_addr("192.168.0.162"); //server ip
+	server.sin_family = AF_NET;
+	server.sin_port = htons(22);
+
+	//connect to remote server
+	if(connect(socket_desc, (struct sockaddr *)&server, sizeof(server))<0)
+	{
+		puts("connect error");
+		return 1;
+	}
+
+	puts("connected");
+	//send some data
+	message ="connect";
+	if(send(socket_desc, message, strlen(message), 0)<0)
+	{
+		puts("send failed");
+		return 1;
+	}
+	puts("data send");
+	//receive reply from server
+	if(recv(socket_desc, server_reply, 2000, 0)<0)
+	{
+		puts("receive failed");
+		return 1;
+	}
+	puts("receive");
+	return 0;
+}
