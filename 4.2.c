c63427d67e0a082aa093cced1075e39740052ed6
Nur <2020975359@isiswa.uitm.edu.my>
2020-11-23 03:29:05 +0000

    Commit for 4.2

--- 4.2.c
+++ 4.2.c
@@ -17,21 +17,21 @@ int main(int argc, char *argv[])
 	
 	//prepare the sock_addr structure
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADRR_ANY;
+	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_port = htons(8888);
 	
 	//bind
-	if(bind(socket_desc,(struct sockadddr *)&server, sizeof(server))<0)
+	if(bind(socket_desc,(struct sockaddr *)&server, sizeof(server))<0)
 	{
 		puts("bind failed");
 	}
 	puts("bind done");
 	
 	//listen
-	listen(socket_dec, 3);
+	listen(socket_desc, 3);
 	
 	//accept incoming connection
-	put("waiting for incoming connections");
+	puts("waiting for incoming connections");
 	c=sizeof(struct sockaddr_in);
 	new_socket=accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
 	if(new_socket<0)
